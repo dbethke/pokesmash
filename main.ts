3cc32826add19a46afff7cc8a5dab79d028a62de
Daniel Bethke <daniel.bethke@gmail.com>
2020-05-25 23:05:26 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,5 @@
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
-    Pizza.destroy()
-    info.changeLifeBy(1)
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    Sharpedo.destroy()
-    info.changeScoreBy(1)
-    Sludge_bomb.destroy(effects.fire, 500)
-})
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    Sludge_bomb = sprites.createProjectileFromSprite(img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . c c c c c . . . . . . . 
-. . . c c c c c c c c . . . . . 
-. . c c c a a c a a c c . . . . 
-. . c c c c c c c a a c c . . . 
-. . c a a c a a a c a a c . . . 
-. c c a c c c c a c c c . . . . 
-. c c c c a a c a c c c . . . . 
-. . c c c a c c c c c . . . . . 
-. . c c c c c a c c . . . . . . 
-. . . c c a a c c . . . . . . . 
-. . . . . c . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, Weezing, 63, 0)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    info.changeLifeBy(-1)
-})
-let Sludge_bomb: Sprite = null
-let Sharpedo: Sprite = null
-let Pizza: Sprite = null
-let Weezing: Sprite = null
-Weezing = sprites.create(img`
+function newPlayer () {
+    Weezing = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . b b . . . f f f f f . . 
 . . . . . . . . . . . . b b . . f b b b b b f . 
@@ -60,31 +25,85 @@ Weezing = sprites.create(img`
 . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Player)
-Weezing.setFlag(SpriteFlag.StayInScreen, true)
-info.setLife(5)
-controller.moveSprite(Weezing)
-game.onUpdateInterval(500, function () {
-    Sharpedo = sprites.create(img`
-. . . . . . . . . . . . . . . . . . . . c c f f f . . . . . . . . . . . 
-. . . . . . . . . . f f f f f f f f f c c c c c f . . . . . . . . . . . 
-. . . . . . . . . f c c c c c c c c c f f f c f . . . . . . . . . . . . 
-. . . . . . . . . f c c c c c c c f c c c c f f . . . . . . . . . . . . 
-. . . . . . . . . f c c c c c c f 2 c c c c c c c c . . . . . . . . . . 
-. . . . . . . . . f c f f f f c c c c f f f c f f c c . . . . . . . . . 
-. . . . . . . . . . f f 1 f 1 c c c f f c f c c f c c c . . . c c c c c 
-. . . . . . . . . . . . f 3 3 3 c f f c f f c f f c c c c c c c c c c c 
-. . . . . . . . . . . f 3 3 3 c c f c c c c c c c c c c c c c c c c c . 
-. . . . . . . . . . . f 3 3 1 c c c c c c c c c c c c c c c c c c c . . 
-. . . . . . . . . . f f f 3 c c c c c c c c c c c c c c f f c c c f . . 
-. . . . . . . . . . f f f f f f f c c c c c c c c c c c c . f c c f . . 
-. . . . . . . . . . . f f f f f f c c c c c c c c c c c . . f c c c f . 
-. . . . . . . . . . . . . f f f f f f f c f f c c c c . . . . f c c c . 
-. . . . . . . . . . . . . . . . . . f f f f f f c c . . . . . . f c c f 
-. . . . . . . . . . . . . . . . . . . f f f f f . . . . . . . . . f f f 
-`, SpriteKind.Enemy)
-    Sharpedo.setVelocity(-100, 0)
-    Sharpedo.setPosition(160, Math.randomRange(0, 120))
+    Weezing.startEffect(effects.trail)
+    Weezing.setPosition(20, 60)
+    Weezing.setFlag(SpriteFlag.StayInScreen, true)
+    controller.moveSprite(Weezing)
+}
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    Sludge_bomb = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . c c c c c . . . . . . . 
+. . . . . . c c . c . . . . . . 
+. . c c . a a c a a c c . . . . 
+. . c c c c c c c a a c c . . . 
+. . . a a c a a a c a a c . . . 
+. c . a c c c c a c c c . . . . 
+. c c c c a a c a c c c . . . . 
+. . c c . a c c . . c . . . . . 
+. . c . c c c a c . . . . . . . 
+. . . c c a a c c . . . . . . . 
+. . . . . c . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, Weezing, 75, 0)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    Pizza.destroy(effects.hearts, 100)
+    info.changeLifeBy(1)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    Sludge_bomb.destroy()
+    Sharpedo.destroy(effects.fire, 200)
+    info.changeScoreBy(100)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    Sludge_bomb = sprites.createProjectileFromSprite(img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . c c c c c . . . . . . . 
+. . . . . . c c . c . . . . . . 
+. . c c . a a c a a c c . . . . 
+. . c c c c c c c a a c c . . . 
+. . . a a c a a a c a a c . . . 
+. c . a c c c c a c c c . . . . 
+. c c c c a a c a c c c . . . . 
+. . c c . a c c . . c . . . . . 
+. . c . c c c a c . . . . . . . 
+. . . c c a a c c . . . . . . . 
+. . . . . c . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, Weezing, 75, 0)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    Sharpedo.destroy(effects.fire, 200)
+    Weezing.destroy(effects.fire, 200)
+    scene.cameraShake(4, 1000)
+    info.changeLifeBy(-1)
+    pause(2000)
+    pause(500)
+    newPlayer()
+})
+info.onLifeZero(function () {
+    game.splash("Game Over")
+    game.over(false)
+    effects.starField.endScreenEffect()
+})
+let Sharpedo: Sprite = null
+let Pizza: Sprite = null
+let Sludge_bomb: Sprite = null
+let Weezing: Sprite = null
+effects.starField.startScreenEffect()
+music.playMelody("B A G A G F A C5 ", 120)
+info.setLife(3)
+info.setScore(0)
+game.splash("Get Ready To Rumble")
+pause(1000)
+newPlayer()
 game.onUpdateInterval(5000, function () {
     Pizza = sprites.create(img`
 . . . . . . . . . . . . . b e e b b b b . . . . . . . . . . . . 
@@ -120,6 +139,28 @@ b 5 5 5 1 1 5 d d d d 5 5 d 5 4 4 e e d e . . . . . . . . . . .
 4 1 5 5 4 e e d e . . . . . . . . . . . . . . . . . . . . . . . 
 4 4 e e . . . 4 e . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Food)
-    Pizza.setPosition(160, Math.randomRange(50, 10))
     Pizza.setVelocity(Math.randomRange(0, -180), 0)
+    Pizza.setPosition(160, Math.randomRange(50, 10))
+})
+game.onUpdateInterval(1000, function () {
+    Sharpedo = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . c c f f f . . . . . . . . . . . 
+. . . . . . . . . . f f f f f f f f f c c c c c f . . . . . . . . . . . 
+. . . . . . . . . f c c c c c c c c c f f f c f . . . . . . . . . . . . 
+. . . . . . . . . f c c c c c c c f c c c c f f . . . . . . . . . . . . 
+. . . . . . . . . f c c c c c c f 2 c c c c c c c c . . . . . . . . . . 
+. . . . . . . . . f c f f f f c c c c f f f c f f c c . . . . . . . . . 
+. . . . . . . . . . f f 1 f 1 c c c f f c f c c f c c c . . . c c c c c 
+. . . . . . . . . . . . f 3 3 3 c f f c f f c f f c c c c c c c c c c c 
+. . . . . . . . . . . f 3 3 3 c c f c c c c c c c c c c c c c c c c c . 
+. . . . . . . . . . . f 3 3 1 c c c c c c c c c c c c c c c c c c c . . 
+. . . . . . . . . . f f f 3 c c c c c c c c c c c c c c f f c c c f . . 
+. . . . . . . . . . f f f f f f f c c c c c c c c c c c c . f c c f . . 
+. . . . . . . . . . . f f f f f f c c c c c c c c c c c . . f c c c f . 
+. . . . . . . . . . . . . f f f f f f f c f f c c c c . . . . f c c c . 
+. . . . . . . . . . . . . . . . . . f f f f f f c c . . . . . . f c c f 
+. . . . . . . . . . . . . . . . . . . f f f f f . . . . . . . . . f f f 
+`, SpriteKind.Enemy)
+    Sharpedo.setVelocity(Math.randomRange(-50, -180), 0)
+    Sharpedo.setPosition(160, Math.randomRange(0, 120))
 })
